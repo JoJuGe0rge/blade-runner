094466b53f24f6bf1fd7a868e82853b5ec9daf7f
JoJuGe0rge <jojumon38@gmail.com>
2025-05-12 03:39:07 +0530

    novice 0.0

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and get Blade Runner to work
         </a>
       </header>
     </div>
